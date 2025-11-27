753cdccb7ddfe81acc4161c37d24c9b9a6ec0084
Williammm0011 <williamsu0011@gmail.com>
2025-11-28 00:21:30 +0800

    plot

--- job.sh
+++ job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=ppo_train
 #SBATCH --output=rl_log_%j.txt
-#SBATCH --ntasks=8
+#SBATCH --ntasks=16
 #SBATCH --time=12:00:00
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --partition=standard
 #SBATCH --account=b12901074
 
 
 echo "Job started at $(date)"
-/storage/undergrad/b12901015/miniconda3/envs/meep-env/bin/python train_ppo.py
+/storage/undergrad/b12901074/miniconda3/envs/rl_final/bin/python train_ppo.py
 echo "Job finished at $(date)"
--- plot_csv.py
+++ plot_csv.py
@@ -11,24 +11,40 @@ def plot_chunks(df, chunk_size=400):
         end_idx = min(start_idx + chunk_size, total_rows)
         df_chunk = df.iloc[start_idx:end_idx]
 
-        # plot the 3 ratios in one plot
-        fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-        ax.plot(df_chunk['output_flux_1_ratio'],
-                label='Output Flux 1 Ratio', color='green')
-        ax.plot(df_chunk['output_flux_2_ratio'],
-                label='Output Flux 2 Ratio', color='red')
-        ax.plot(df_chunk['loss_ratio'],
-                label='Loss Ratio', color='purple')
-        ax.set_title(
-            f'Output Flux Ratios over Time (Data points {start_idx+1}-{end_idx})')
-        ax.set_xlabel('Timestamp')
-        ax.set_ylabel('Flux Ratio')
-        ax.tick_params(axis='x', rotation=45)
-        ax.legend()
+        # plot 'reward' and 'score' in one plot
+        fig, ax1 = plt.subplots(1, 1, figsize=(12, 8))
+        
+        ax1.plot(df_chunk['reward'], label='Episode Reward', color='blue')
+        ax1.plot(df_chunk['current_score'], label='Score', color='orange')      
+        ax1.set_title(
+            f'Episode Reward over Time (Data points {start_idx+1}-{end_idx})')
+        ax1.set_xlabel('Timestamp')
+        ax1.set_ylabel('Episode Reward', color='blue')
+        ax1.tick_params(axis='x', rotation=45)
+        ax1.legend()
         plt.tight_layout()
         plt.savefig(
             f'ppo_model_logs/episode_rewards_plot_{start_idx+1}_{end_idx}.png')
         plt.show()
+
+        # plot the 3 ratios in one plot
+        # fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+        # ax.plot(df_chunk['output_flux_1_ratio'],
+        #         label='Output Flux 1 Ratio', color='green')
+        # ax.plot(df_chunk['output_flux_2_ratio'],
+        #         label='Output Flux 2 Ratio', color='red')
+        # ax.plot(df_chunk['loss_ratio'],
+        #         label='Loss Ratio', color='purple')
+        # ax.set_title(
+        #     f'Output Flux Ratios over Time (Data points {start_idx+1}-{end_idx})')
+        # ax.set_xlabel('Timestamp')
+        # ax.set_ylabel('Flux Ratio')
+        # ax.tick_params(axis='x', rotation=45)
+        # ax.legend()
+        # plt.tight_layout()
+        # plt.savefig(
+        #     f'ppo_model_logs/episode_rewards_plot_{start_idx+1}_{end_idx}.png')
+        # plt.show()
 
 
 def plot_multiples_of_50(df):
@@ -57,23 +73,12 @@ def plot_multiples_of_50(df):
 
 
 # read the episode_rewards.csv
-df = pd.read_csv('ppo_model_logs/episode_rewards.csv')
+df = pd.read_csv('ppo_model_logs/episode_rewards_terminated.csv')
 # Strip whitespace from column names
 df.columns = df.columns.str.strip()
 
 # Call the functions
-plot_chunks(df, chunk_size=400)
-plot_multiples_of_50(df)
+plot_chunks(df, chunk_size=800)
+# plot_multiples_of_50(df)
 
-'''
-the csv looks like this:
-timestamp, current_score, reward, output_flux_1_ratio, output_flux_2_ratio, loss_ratio
-20251124_120610, -271150431.5958999, -271150431.5958999, 0.1564002481938243, 0.16247295940209122, 0.6811267924040845
-20251124_120623, -253681031.53752744, 17469400.058372438, 0.12079411958354017, 0.13141430438009444, 0.7477915760363655
-20251124_120636, -227172206.71433473, 26508824.823192716, 0.11219000836689294, 0.09840778672846615, 0.789402204904641
-20251124_120649, -264252119.56256726, -37079912.84823254, 0.0878136821420356, 0.07910912720565456, 0.8330771906523099
-20251124_120702, -316355373.1651194, -52103253.602552146, 0.058630538176213276, 0.04298013500014441, 0.8983893268236423
-20251124_120715, -333186624.4227973, -16831251.257677913, 0.05075852049456323, 0.030976291176804574, 0.9182651883286321
-20251124_120728, -312297974.4459907, 20888649.97680664, 0.05319109506506582, 0.03200790271192431, 0.9148010022230098
-20251124_120741, -286996999.8494451, 25300974.596545577, 0.06382986343810397, 0.04975502061689987, 0.8864151159449961
-'''
+
